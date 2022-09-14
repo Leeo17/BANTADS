c938139084005fea1f2f65f4b28076fa224d50b4
Leeo17 <leeo.oneda@gmail.com>
2022-09-13 21:58:36 -0300

    Alterado bash de run

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 docker-compose -f start-postgres.yml up -d
 sleep 15
 docker-compose -f start-rabbitmq.yml up -d
-sleep 5
+sleep 10
 docker-compose -f start-orchestrator.yml up -d
 docker-compose -f start-services.yml up -d
